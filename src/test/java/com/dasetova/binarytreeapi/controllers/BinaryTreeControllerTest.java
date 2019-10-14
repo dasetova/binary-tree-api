d463446ba9c02fdba65d65b7e199865a5b4b0bdd
dasetova <dasetova@gmail.com>
2019-10-14 11:09:57 -0500

    getLowestCommonAncestor EndPoint implementation

--- src/test/java/com/dasetova/binarytreeapi/controllers/BinaryTreeControllerTest.java
+++ src/test/java/com/dasetova/binarytreeapi/controllers/BinaryTreeControllerTest.java
@@ -1,7 +1,10 @@
 package com.dasetova.binarytreeapi.controllers;
 
+import static org.mockito.Mockito.when;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -13,6 +16,9 @@ import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import com.dasetova.binarytreeapi.exceptions.NotFoundException;
+import com.dasetova.binarytreeapi.models.Node;
+import com.dasetova.binarytreeapi.repositories.BinaryTreeRepository;
 import com.dasetova.binarytreeapi.services.BinaryTreeService;
 
 /**
@@ -29,6 +35,9 @@ public class BinaryTreeControllerTest {
 	
 	@MockBean
 	private BinaryTreeService treeService;
+	
+	@MockBean
+	private BinaryTreeRepository treeRepository;
 	
 	@Autowired
 	BinaryTreeController treeController;
@@ -60,6 +69,25 @@ public class BinaryTreeControllerTest {
 		String request = String.format("{\"treeSize\": %s, \"maxValue\" : %s}", treeSize, maxValue);
 	    mockMvc.perform(MockMvcRequestBuilders.post("/binary_trees")
 	      .content(request)
+	      .contentType(MediaType.APPLICATION_JSON_UTF8))
+	      .andExpect(resultStatus);
+	}
+	
+	@Test
+	public void whenSendingTreeIdToGetLCA_thenBadRequestResponse() throws Exception {
+		when(treeService.getLowestCommonAncestor(Mockito.eq(-1), Mockito.anyInt(), Mockito.anyInt())).thenThrow(NotFoundException.class);
+		performGetLowestCommonAncestorRequest(-1, 10, 100, MockMvcResultMatchers.status().isNotFound());
+	}
+	
+	@Test
+	public void whenSendingValidParamsToGetLCA_thenOkResponse() throws Exception {
+		when(treeService.getLowestCommonAncestor(Mockito.anyInt(), Mockito.anyInt(), Mockito.anyInt())).thenReturn(new Node(10));
+		performGetLowestCommonAncestorRequest(1, 10, 100, MockMvcResultMatchers.status().isOk());
+	}
+	
+	private void performGetLowestCommonAncestorRequest(Integer treeId, Integer value1, Integer value2, ResultMatcher resultStatus) throws Exception {
+		String url = String.format("/binary_trees/%s/lowestCommonAncestor?value1=%s&value2=%s", treeId, value1, value2);
+		mockMvc.perform(MockMvcRequestBuilders.get(url)
 	      .contentType(MediaType.APPLICATION_JSON_UTF8))
 	      .andExpect(resultStatus);
 	}
